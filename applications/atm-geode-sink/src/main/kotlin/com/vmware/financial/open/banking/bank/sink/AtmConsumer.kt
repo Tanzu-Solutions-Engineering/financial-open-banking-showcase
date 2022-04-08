585ae1cf9d436d2451e71156dce7e764fdd39623
Gregory Green <green_gregory@yahoo.com>
2022-04-07 20:43:44 -0400

    CDC for ATM

--- applications/atm-geode-sink/src/main/kotlin/com/vmware/financial/open/banking/bank/sink/AtmConsumer.kt
+++ applications/atm-geode-sink/src/main/kotlin/com/vmware/financial/open/banking/bank/sink/AtmConsumer.kt
@@ -2,6 +2,7 @@ package com.vmware.financial.open.banking.bank.sink
 
 import com.vmware.financial.open.banking.atm.domain.Atm
 import com.vmware.financial.open.banking.atm.service.AtmService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.function.Consumer
 
@@ -10,6 +11,7 @@ import java.util.function.Consumer
  */
 @Service
 class AtmConsumer(private val atmService: AtmService) : Consumer<Atm> {
+
     override fun accept(atm: Atm) {
         atmService.createAtm(atm.bank_id,atm)
     }
--- components/atm-service/src/main/kotlin/com/vmware/financial/open/banking/atm/service/AtmDataService.kt
+++ components/atm-service/src/main/kotlin/com/vmware/financial/open/banking/atm/service/AtmDataService.kt
@@ -1,6 +1,8 @@
 package com.vmware.financial.open.banking.atm.service
 
 import com.vmware.financial.open.banking.atm.domain.Atm
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.data.gemfire.GemfireTemplate
 import org.springframework.stereotype.Service
 import java.util.*
@@ -10,8 +12,14 @@ import java.util.*
  */
 @Service
 class AtmDataService(private val gemfireTemplate: GemfireTemplate) : AtmService {
+
+    var logger: Logger = LoggerFactory.getLogger(AtmDataService::class.java)
+
     override fun createAtm(bankId: String, atm: Atm): Atm {
-        gemfireTemplate.put("${bankId}|${atm.id}",atm)
+        gemfireTemplate.put("$bankId|${atm.id}",atm)
+
+        logger.info("Saving key: $bankId|${atm.id} ATM: $atm")
+
         return atm
     }
 
