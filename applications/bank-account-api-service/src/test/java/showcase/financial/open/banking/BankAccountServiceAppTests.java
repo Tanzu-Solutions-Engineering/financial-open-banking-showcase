a55905f2c7fbe274df268207009ae3b65ff76c72
ggreen <gregoryg@vmware.com>
2024-05-29 10:06:10 -0400

    Remove spring boot test to required gemfire when testing

--- applications/bank-account-api-service/src/test/java/showcase/financial/open/banking/BankAccountServiceAppTests.java
+++ applications/bank-account-api-service/src/test/java/showcase/financial/open/banking/BankAccountServiceAppTests.java
@@ -3,10 +3,10 @@ package showcase.financial.open.banking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BankAccountServiceAppTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
