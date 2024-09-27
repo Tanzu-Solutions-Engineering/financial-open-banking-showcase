54f1dfc9a624df4fd066f5264d04d62be5f29f12
ggreen <gregoryg@vmware.com>
2024-09-27 10:09:57 -0400

    Adding credit supplier

--- applications/transaction-supplier/src/main/java/showcase/financial/open/banking/transaction/supplier/AppConfig.java
+++ applications/transaction-supplier/src/main/java/showcase/financial/open/banking/transaction/supplier/AppConfig.java
@@ -2,7 +2,10 @@ package showcase.financial.open.banking.transaction.supplier;
 
 import lombok.extern.slf4j.Slf4j;
 import nyla.solutions.core.patterns.creational.Creator;
+import nyla.solutions.core.patterns.creational.generator.FullNameCreator;
 import nyla.solutions.core.patterns.creational.generator.JavaBeanGeneratorCreator;
+import nyla.solutions.core.patterns.creational.generator.PickRandomTextCreator;
+import nyla.solutions.core.util.Digits;
 import org.springframework.cloud.function.context.config.JsonMessageConverter;
 import org.springframework.cloud.function.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +19,7 @@ import java.util.function.Supplier;
 @Slf4j
 public class AppConfig {
 
+
     @Bean
     MessageConverter messageConverter(JsonMapper jsonMapper)
     {
@@ -23,9 +27,32 @@ public class AppConfig {
     }
 
     @Bean
-    Creator<Transaction> creator()
+    Digits digits()
     {
-        return JavaBeanGeneratorCreator.of(Transaction.class);
+        return new Digits();
+    }
+
+    @Bean
+    Creator<Transaction> creator(Digits digits)
+    {
+        short minYear = 2024;
+        short maxYear = 2035;
+
+        short minMonth = 1;
+        short maxMonth = 12;
+        return new JavaBeanGeneratorCreator(Transaction.class)
+                .generateNestedAll()
+                .randomizeAll()
+                .creatorForProperty("name_on_card", new FullNameCreator())
+                .creatorForProperty("expiry_year",  () -> digits.generateShort(minYear,maxYear))
+                .creatorForProperty("expiry_month",  () -> digits.generateShort(minMonth,maxMonth))
+                .creatorForProperty("brand",  PickRandomTextCreator.options("Amex",
+                        "Discover","Wells Fargo","Citigroup","Chase","Barclays",
+                        "MasterCard","PNC Bank","Visa","Navy Federal Credit Union"))
+                .creatorForProperty("card_type",  PickRandomTextCreator.options("Amex",
+                        "Rewards","Cashback","Travel","Business","Corporate"))
+                .creatorForProperty("description",PickRandomTextCreator.options("issued by a financial company",
+                        " minimum payment each period","Summary of account activity","basic terms that you may come across when using your credit card."));
     }
 
     @Bean
--- components/transaction-domain/src/main/java/showcase/financial/open/banking/transactions/Transaction.java
+++ components/transaction-domain/src/main/java/showcase/financial/open/banking/transactions/Transaction.java
@@ -1,8 +1,12 @@
 package showcase.financial.open.banking.transactions;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
      private TransactionCard card;
      private TransactionTo to;
